7308b2106642d5efab4923fa81c5a31581829bc2
Maxime Boucher <maxime@maximeboucher.com>
2026-07-26 11:27:59 -0600

    Add theme toggle to navbar

--- components/icon-link.tsx
+++ components/icon-link.tsx
@@ -8,11 +8,13 @@ interface IconLink {
 
 export default function IconLink({ href, name, icon }: IconLink) {
   return (
-    <a href={href} aria-label={name} target="_blank">
-      {React.cloneElement(icon, {
-        className:
-          "size-6 hover:scale-110 transition-all duration-200 ease-out text-foreground opacity-80 hover:opacity-100",
-      })}
+    <a
+      href={href}
+      aria-label={name}
+      target="_blank"
+      className="block text-foreground opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-200 ease-out"
+    >
+      {React.cloneElement(icon, { className: "size-6" })}
     </a>
   );
 }
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,33 +1,40 @@
 import Logo from "../logo";
 import IconLink from "../icon-link";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { IoIosDocument } from "react-icons/io";
+import { DocumentFill, Github2Fill, LinkedinFill } from "@mingcute/react";
+import { AnimatedThemeToggler } from "../ui/animated-theme-toggler";
 
 export default function Navbar() {
   return (
     <header className="py-4 sm:py-6 px-5 sm:px-8 flex justify-between items-center absolute lg:fixed z-20 inset-x-0 top-0">
       <Logo />
       <div>
-        <ul className="flex justify-start items-center gap-3">
+        <ul className="flex justify-start items-center gap-2.5">
           <li>
             <IconLink
               href="https://github.com/themaxboucher"
               name="Github"
-              icon={<FaGithub />}
+              icon={<Github2Fill />}
             />
           </li>
           <li>
             <IconLink
               href="https://www.linkedin.com/in/maxboucher/"
               name="LinkedIn"
-              icon={<FaLinkedin />}
+              icon={<LinkedinFill />}
             />
           </li>
           <li>
             <IconLink
               href="https://drive.google.com/file/d/1H7409USL8zmPCu1lcsioViUcJWMpcpSU/view?usp=sharing"
               name="Resume"
-              icon={<IoIosDocument />}
+              icon={<DocumentFill />}
+            />
+          </li>
+          <div className="h-6 min-w-0.5 bg-border rounded-2xl"></div>
+          <li>
+            <AnimatedThemeToggler
+              aria-label="Toggle theme"
+              className="size-6 block cursor-pointer text-foreground opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-200 ease-out"
             />
           </li>
         </ul>
