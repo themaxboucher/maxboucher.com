983832c50e316065744a32d2ff8db98b642346b9
Maxime Boucher <maxime@maximeboucher.com>
2026-07-25 18:52:22 -0600

    Remove background grid

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import {
   Source_Code_Pro as FontMono,
 } from "next/font/google";
 import { cn } from "@/lib/utils";
-import AnimatedGridPattern from "@/components/ui/animated-grid-pattern";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -34,7 +33,7 @@ export default function RootLayout({
         className={cn(
           "min-h-screen max-w-full overflow-x-hidden bg-background font-sans antialiased",
           fontSans.variable,
-          fontMono.variable
+          fontMono.variable,
         )}
       >
         <ThemeProvider
@@ -43,16 +42,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="absolute inset-0 -z-50 h-[70vh] w-full bg-gradient-to-br from-muted/10 to-transparent"></div>
-          <AnimatedGridPattern
-            numSquares={20}
-            maxOpacity={0.05}
-            duration={3}
-            repeatDelay={1}
-            className={cn("absolute inset-x-0 h-[70vh] -z-40")}
-          />
-          <div className="absolute inset-0 -z-30 h-[70vh] w-full bg-gradient-to-t from-background to-transparent to-50%"></div>
-
           {children}
         </ThemeProvider>
       </body>
